557c7592c688813948c3d8d9d3e92f5a4b315f03
Lukas M <lukas.mrtvy@gmail.com>
2019-02-13 21:53:04 +0100

    Update exec.sh

--- scripts/exec.sh
+++ scripts/exec.sh
@@ -21,7 +21,7 @@ then
   options+=(-u $AUTH_USER:$AUTH_PASSWORD)
 fi
 
-curl -sSL "${options[@]}" $URL/api/providers/docker/frontends  | jq -r ' .[].routes | to_entries[] | .value.rule | gsub("Host:";"https://")' -o $workdir/results.json > /dev/null 2>&1
+curl -sSL "${options[@]}" $URL/api/providers/docker/frontends  | jq -r ' .[].routes | to_entries[] | .value.rule | gsub("Host:";"https://")' > $workdir/results.json
 
 echo "$(date -I'seconds') INFO Loading data from Traefik API DONE"
 
