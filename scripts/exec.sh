04b2530aca8c41aba278cdc7968c9a38c4286f4e
Lukas M <lukas.mrtvy@gmail.com>
2019-02-13 20:47:26 +0100

    Create exec.sh

---
+++ scripts/exec.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo "$(date -I'seconds') INFO Loading data Traefik API"
+
+URL="${URL:-http://traefik:8080}"
+
+if [ $CUSTOM_CA ];
+then
+  options+=(--cacert $cacert_path)
+fi
+
+if [ $SKIP_SSL_VERIFICATION ];
+then
+  options+=(-k)
+fi
+
+if [ $AUTH_USER ] && [ $AUTH_PASSWORD ];
+then
+  options+=(-u $AUTH_USER:$AUTH_PASSWORD)
+fi
+
+curl -sSL "${options[@]}" $URL/api/providers/docker/frontends  | jq -r ' .[].routes | to_entries[] | .value.rule | gsub("Host:";"https://")'  > /dev/null 2>&1
+
+
+echo "$(date -I'seconds') INFO Loading data from Traefik API DONE"
+
