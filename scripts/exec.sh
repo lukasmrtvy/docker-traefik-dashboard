f7fbe6f4d1876a23478d649ea12349ddcf7946ec
Lukas M <lukas.mrtvy@gmail.com>
2019-02-16 19:44:27 +0100

    Update exec.sh

--- scripts/exec.sh
+++ scripts/exec.sh
@@ -5,6 +5,7 @@ workdir=/opt/check/
 echo "$(date -I'seconds') INFO Loading data Traefik API"
 
 URL="${URL:-http://traefik:8080}"
+PROVIDER="${PROVIDER:-docker}"
 
 if [ "$CUSTOM_CA" ];
 then
@@ -19,6 +20,6 @@ then
   options+=(-u $AUTH_USER:$AUTH_PASSWORD)
 fi
 
-curl -sSL "${options[@]}" $URL/api/providers/docker/frontends  | jq '{data: map([.routes[].rule | sub("Host:"; "https://") | split(";")[0] ]) | unique | sort}' > $workdir/results.json
+curl -sSL "${options[@]}" $URL/api/providers/$PROVIDER/frontends  | jq '{data: map([.routes[].rule | sub("Host:"; "https://") | split(";")[0] ]) | unique | sort}' > $workdir/results.json
 
 echo "$(date -I'seconds') INFO Loading data from Traefik API DONE"
